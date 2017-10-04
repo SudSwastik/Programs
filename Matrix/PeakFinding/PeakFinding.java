58f02bf5297241199b023357b8125731e39bf38b
himanshu4746 <himanshu4746@gmail.com>
2017-10-04 17:45:59 +0530

    Local Maxima in 2-D Array

--- Matrix/PeakFinding/PeakFinding.java
+++ Matrix/PeakFinding/PeakFinding.java
@@ -1,4 +1,5 @@
 package peakfinding;
+
 import java.util.*;
 import java.io.*;
 /**
@@ -6,47 +7,56 @@ import java.io.*;
  * @author Himanshu
  */
 public class PeakFinding {
-    private int r;
-    private int c;
-    int findPeak(int[][] arr, int lCol, int rCol){
-        int midCol = (lCol+rCol)/2, max = Integer.MIN_VALUE,maxRowIndex=0;
+    private final int r;
+    private final int c;
+    private final int[][] arr;
+    private int midCol;
+    private int maxRowIndex;
+    PeakFinding(int r, int c){
+        this.r = r;
+        this.c = c;
+        this.arr = new int[r][c];
+    }
+    int findPeak(int lCol, int rCol){
+        this.midCol = (lCol+rCol)/2; 
+        int max = Integer.MIN_VALUE;
+        this.maxRowIndex=0;
         for(int i=0; i< this.r; i++){
-            if(arr[i][midCol]>max){
-                max=arr[i][midCol];
-                maxRowIndex=i;
+            if(this.arr[i][midCol]>max){
+                max=this.arr[i][midCol];
+                this.maxRowIndex=i;
             }
-            System.out.println(maxRowIndex);
         }
         if(lCol<=rCol){
             if(c==1)
                 return max;
-            else if(midCol==0 && arr[maxRowIndex][midCol] >= arr[maxRowIndex][midCol+1])
-                return arr[maxRowIndex][midCol];
-            else if(midCol==c-1 && arr[maxRowIndex][midCol] >= arr[maxRowIndex][midCol-1])
-                return arr[maxRowIndex][midCol];
-            else if(midCol!=0 && midCol!=c-1 && arr[maxRowIndex][midCol] >= arr[maxRowIndex][midCol-1] && arr[maxRowIndex][midCol] >= arr[maxRowIndex][midCol+1])
-                return arr[maxRowIndex][midCol];
-            else if(midCol!=0 && /*midCol!=c-1 &&*/ arr[maxRowIndex][midCol-1] > arr[maxRowIndex][midCol])
-                return findPeak(arr,lCol, midCol-1);
-            else if(/*midCol!=0 &&*/ midCol!=c-1 && arr[maxRowIndex][midCol+1] > arr[maxRowIndex][midCol])
-                return findPeak(arr,midCol+1, rCol);
+            else if(midCol==0 && this.arr[maxRowIndex][midCol] >= this.arr[maxRowIndex][midCol+1])
+                return this.arr[maxRowIndex][midCol];
+            else if(midCol==c-1 && this.arr[maxRowIndex][midCol] >= this.arr[maxRowIndex][midCol-1])
+                return this.arr[maxRowIndex][midCol];
+            else if(midCol!=0 && midCol!=c-1 && this.arr[maxRowIndex][midCol] >= this.arr[maxRowIndex][midCol-1] && this.arr[maxRowIndex][midCol] >= this.arr[maxRowIndex][midCol+1])
+                return this.arr[maxRowIndex][midCol];
+            else if(midCol!=0 && this.arr[maxRowIndex][midCol-1] > this.arr[maxRowIndex][midCol])
+                return findPeak(lCol, midCol-1);
+            else if(midCol!=c-1 && this.arr[maxRowIndex][midCol+1] > this.arr[maxRowIndex][midCol])
+                return findPeak(midCol+1, rCol);
             else
                 return -1;
         }
         return -1; 
     }
     public static void main(String[] args) {
-        PeakFinding obj = new PeakFinding();
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter no of Rows: ");
-        obj.r = scanner.nextInt();
+        int r = scanner.nextInt();
         System.out.print("Enter no of Columns: ");
-        obj.c = scanner.nextInt();
-        int[][] arr = new int[obj.r][obj.c];
+        int c = scanner.nextInt();
+        PeakFinding obj = new PeakFinding(r,c);
         System.out.println("Enter Matrix: ");
         for(int i=0; i<obj.r; i++)
             for(int j=0;j<obj.c; j++)
-                arr[i][j] = scanner.nextInt();
-        System.out.println("Peak: " + obj.findPeak(arr,0,obj.c-1));
+                obj.arr[i][j] = scanner.nextInt();
+        int result = obj.findPeak(0,obj.c-1);
+        System.out.println("Peak arr["+obj.maxRowIndex+"]["+obj.midCol+"]: " + result);
     }
 }
